d111f26ae596a85b967bc13aaee79f2ea9fd093b
John Rodney <johnrodney84@gmail.com>
2018-07-06 04:20:57 -0500

    linting must be done

---
+++ src/components/category-list.jsx
@@ -0,0 +1,135 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Category from './category';
+import Place from './place';
+
+const modes = ['category-view', 'category-places'];
+
+export default class CategoryList extends Component {
+  constructor() {
+    super();
+
+    this.changePlace = this.changePlace.bind(this);
+    this.state = {
+      mode: modes[0],
+    };
+  }
+
+  getPlaceTypes() {
+    const { mode } = this.state;
+    const { placeTypes } = this.props;
+
+    if (mode !== modes[0]) return <div />;
+
+    return placeTypes.map(place => (
+      <div>
+        <Category
+          changePlace={this.changePlace}
+          place={place}
+        />
+      </div>
+    ));
+  }
+
+  getCategoryHeader() {
+    const { mode } = this.state;
+    const { activeType } = this.props;
+
+    if (mode !== modes[1]) return <div />;
+
+    return (
+      <button
+        type="button"
+        className="category-header"
+        onClick={() => this.setState({ mode: modes[0] })}
+      >
+        <i className="material-icons category-header-icon">chevron_left</i>
+        <h1>{activeType}</h1>
+      </button>
+    );
+  }
+
+  getPlaces() {
+    const { mode } = this.state;
+    const { places } = this.props;
+
+    if (mode !== modes[1]) return <div />;
+
+    return (
+      <div className="places-list">
+        {
+          places.map((place, i) => (
+            i < 10 ? (
+              <div className="a-place">
+                <Place place={place} />
+              </div>) : ''
+          ))
+        }
+      </div>
+    );
+  }
+
+  changePlace(place) {
+    const { changePlace } = this.props;
+    const mode = modes[1];
+
+    changePlace(place);
+    this.setState({ mode });
+  }
+
+  render() {
+    const placeTypesList = this.getPlaceTypes();
+    const placesList = this.getPlaces();
+    const categoryHeader = this.getCategoryHeader();
+
+    return (
+      <div>
+        { categoryHeader }
+        { placesList }
+        { placeTypesList }
+      </div>
+    );
+  }
+}
+
+CategoryList.propTypes = {
+  changePlace: PropTypes.func.isRequired,
+  placeTypes: PropTypes.arrayOf(PropTypes.string.isRequired).isRequired,
+  activeType: PropTypes.string.isRequired,
+  places: PropTypes.arrayOf(PropTypes.shape({
+    formatted_address: PropTypes.string.isRequired,
+    geometry: PropTypes.shape({
+      location: PropTypes.shape({
+        lat: PropTypes.number.isRequired,
+        lng: PropTypes.number.isRequired,
+      }).isRequired,
+      viewport: PropTypes.shape({
+        east: PropTypes.number.isRequired,
+        north: PropTypes.number.isRequired,
+        south: PropTypes.number.isRequired,
+        west: PropTypes.number.isRequired,
+      }).isRequired,
+    }).isRequired,
+    html_attributions: PropTypes.arrayOf(PropTypes.any).isRequired,
+    icon: PropTypes.string.isRequired,
+    id: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    opening_hours: PropTypes.shape({
+      open_now: PropTypes.bool.isRequired,
+    }).isRequired,
+    photos: PropTypes.arrayOf(PropTypes.shape({
+      height: PropTypes.number.isRequired,
+      html_attributions: PropTypes.arrayOf(PropTypes.string.isRequired).isRequired,
+      width: PropTypes.number.isRequired,
+    }).isRequired).isRequired,
+    place_id: PropTypes.string.isRequired,
+    plus_code: PropTypes.shape({
+      compound_code: PropTypes.string.isRequired,
+      global_code: PropTypes.string.isRequired,
+    }).isRequired,
+    price_level: PropTypes.number.isRequired,
+    rating: PropTypes.number.isRequired,
+    reference: PropTypes.string.isRequired,
+    types: PropTypes.arrayOf(PropTypes.string.isRequired).isRequired,
+  }).isRequired).isRequired,
+};
--- src/components/map.jsx
+++ src/components/map.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import defaultMapStyles from '../config/map-styles';
 import categoryToIconMap from '../config/icon-category-map';
 import CategoryList from './category-list';
@@ -11,18 +12,33 @@ export default class MapComponent extends React.Component {
     const { config } = window;
 
     this.markers = [];
+    this.changePlace = this.changePlace.bind(this);
     this.state = {
       placeTypes: config.defaultTypes,
       loading: false,
       activeType: config.defaultTypes[0],
       places: [],
-    }
+    };
+  }
+
+  componentDidMount() {
+    const { config } = window;
+
+    this.initMapFromConfig(config.defaultAddress);
+  }
+
+  setStyles() {
+    const styles = defaultMapStyles;
+    const { placeTypes } = this.state;
+
+    this.map.setOptions({ styles });
+    this.findLocalPlaces(placeTypes[0]);
   }
 
   initMapFromConfig(address) {
     const geocoder = new google.maps.Geocoder();
 
-    geocoder.geocode({ address }, (res, status) => {
+    geocoder.geocode({ address }, (res) => {
       this.makeMapFromLocation(res[0].geometry.location, res[0]);
     });
   }
@@ -35,7 +51,7 @@ export default class MapComponent extends React.Component {
     this.map = new google.maps.Map(document.getElementById('map'), {
       center: location,
       disableDefaultUI: true,
-      zoom: 13
+      zoom: 13,
     });
 
     this.infoWindow = new google.maps.InfoWindow();
@@ -52,8 +68,6 @@ export default class MapComponent extends React.Component {
   }
 
   addMarker(location, title, iconType, place) {
-    const { Size, Point } = google.maps;
-
     const marker = new google.maps.Marker({
       map: this.map,
       position: location,
@@ -62,7 +76,7 @@ export default class MapComponent extends React.Component {
         fontFamily: 'Material Icons',
         color: 'white',
         text: iconType,
-      }
+      },
     });
 
     google.maps.event.addListener(marker, 'click', () => {
@@ -74,7 +88,8 @@ export default class MapComponent extends React.Component {
           <p>${place.opening_hours && place.opening_hours.open_now ? 'open' : 'closed'}<p>
           <p>${place.rating} stars<p>
         </div>
-      `
+      `;
+
       this.infoWindow.setContent(content);
       this.infoWindow.open(this.map, marker);
     });
@@ -83,78 +98,74 @@ export default class MapComponent extends React.Component {
   }
 
   findLocalPlaces(query) {
-    this.setState({ loading: true })
+    this.setState({ loading: true });
 
     const service = new google.maps.places.PlacesService(this.map);
     const { location } = this;
     const request = {
       location,
       rankBy: google.maps.places.RankBy.DISTANCE,
-      query
+      query,
     };
 
     service.textSearch(request, this.addMarkersFromNearbySearchResult.bind(this));
   }
 
   addMarkersFromNearbySearchResult(res) {
-    const icon = categoryToIconMap[this.state.activeType];
+    const { activeType } = this.state;
+    const icon = categoryToIconMap[activeType];
 
     res.forEach(result => this.addMarker(result.geometry.location, result.name, icon, result));
-    this.setState({ loading: false, places: res })
-  }
-
-  setStyles() {
-    const styles = defaultMapStyles;
-
-    this.map.setOptions({ styles })
-    this.findLocalPlaces(this.state.placeTypes[0]);
-  }
-
-  componentDidMount() {
-    const { config } = window;
-
-    this.initMapFromConfig(config.defaultAddress);
+    this.setState({ loading: false, places: res });
   }
 
   changePlace(place) {
     const activeType = place.toLowerCase();
 
     this.clearMarkers();
-    this.setState({ activeType })
+    this.setState({ activeType });
     this.findLocalPlaces(place);
   }
 
   clearMarkers() {
-    this.markers.forEach(marker => marker.setMap(null))
+    this.markers.forEach(marker => marker.setMap(null));
     this.markers = [];
     this.addDefaultLocationMarker();
   }
 
   render() {
-    const loader = this.state.loading ? <div className="loader">Loading...</div> : '';
+    const {
+      loading, placeTypes, activeType, places,
+    } = this.state;
+    const loader = loading ? <div className="loader" /> : '';
     const { close } = this.props;
 
     return (
       <div className="map-component-modal">
-        <div
+        <button
+          type="button"
           className="close-map"
-          onClick={() => this.props.close()}
+          onClick={() => close()}
         >
-        <i class="material-icons">close</i>
-        </div>
+          <i className="material-icons">close</i>
+        </button>
         { loader }
         <div className="map-modal-content">
           <div className="places-menu">
             <CategoryList
-              placeTypes={this.state.placeTypes}
-              changePlace={this.changePlace.bind(this)}
-              activeType={this.state.activeType}
-              places={this.state.places}
+              placeTypes={placeTypes}
+              changePlace={this.changePlace}
+              activeType={activeType}
+              places={places}
             />
           </div>
-          <div className="map" id="map"></div>
+          <div className="map" id="map" />
         </div>
       </div>
     );
   }
 }
+
+MapComponent.propTypes = {
+  close: PropTypes.func.isRequired,
+};
